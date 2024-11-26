d29cdda369fe4f3785249239ded08efdfcd482b0
Ketlen Vasconcelos Almeida <161050069+ketlens@users.noreply.github.com>
2024-11-26 01:21:30 +0000

    [feat] Banco de dados do pagamento feito

--- src/database/initializeDatabase.js
+++ src/database/initializeDatabase.js
@@ -1,8 +1,9 @@
 export async function initializeDatabase(database) {
     try {
         await database.execAsync(`
-            DROP TABLE IF EXISTS users;
+            DROP TABLE IF EXISTS payments;
 
+            DROP TABLE IF EXISTS users;
 
             CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,6 +15,16 @@ export async function initializeDatabase(database) {
                 updated_at DATE
             );
 
+            CREATE TABLE IF NOT EXISTS payments (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
+                valor_pago REAL NOT NULL,
+                data_pagamento DATE NOT NULL,
+                observacao TEXT,
+                created_at DATE DEFAULT CURRENT_TIMESTAMP,
+                updated_at DATE,
+                FOREIGN KEY (user_id) REFERENCES users (id)
+            );
 
             INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Super', 'super@email.com', 'A123456a!', 'SUPER');
             INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Admin', 'admin@email.com', 'A123456a!', 'ADMIN');
